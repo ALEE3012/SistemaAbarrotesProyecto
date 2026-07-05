5b685ceaab8245e628572bf5d377bde2547cbd15
LENOVO <LENOVO@192.168.3.109>
2026-07-05 18:06:07 -0500

    Upgrade

--- src/java/Utilidades/Conexion.java
+++ src/java/Utilidades/Conexion.java
@@ -5,27 +5,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
-
-    private static final String URL = "jdbc:mysql://mysql.railway.internal:3306/BD_Abarrotes";
+    private static final String URL = "jdbc:mysql://thomas.proxy.rlwy.net:26121/BD_Abarrotes";
     private static final String USER = "root";
-    private static final String PASS = "vjYerGOFnXYpiBUVXovnLrVXdfkagavN";
+    private static final String PASS = "vjYerGOFnXYpiBUVXovnLrVXdfkagavN"; // cámbiala después de probar
 
     public static Connection conectar() {
-
         Connection con = null;
-
         try {
-
             con = DriverManager.getConnection(URL, USER, PASS);
             System.out.println("✅ Conexión exitosa");
-
         } catch (SQLException e) {
-
             System.out.println("❌ Error de conexión");
             e.printStackTrace();
-
         }
-
         return con;
     }
 }
