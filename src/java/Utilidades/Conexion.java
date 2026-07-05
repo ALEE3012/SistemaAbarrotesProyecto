e9a9dea57793cb17e68841bd522a0076e8361c7c
LENOVO <LENOVO@192.168.3.109>
2026-07-05 17:36:59 -0500

    Upgrade

--- src/java/Utilidades/Conexion.java
+++ src/java/Utilidades/Conexion.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class Conexion {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/BD_Abarrotes";
+    private static final String URL = "jdbc:mysql://mysql.railway.internal:3306/BD_Abarrotes";
     private static final String USER = "root";
-    private static final String PASS = "301223";
+    private static final String PASS = "vjYerGOFnXYpiBUVXovnLrVXdfkagavN";
 
     public static Connection conectar() {
 
